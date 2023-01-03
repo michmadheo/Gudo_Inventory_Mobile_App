4f1c81e66d1f3904ee5a4557658f8f325a79ad48
MichaelAmadheo <michael.amadheo@sera.astra.co.id>
2023-01-03 21:52:36 +0700

    Project init & React Navigation

---
+++ App.tsx
@@ -0,0 +1,18 @@
+import React from 'react';
+import {
+  SafeAreaView,
+  StyleSheet,
+  Text
+} from 'react-native';
+
+const App = () => {
+  return (
+    <SafeAreaView>
+      <Text>Gudo</Text>
+    </SafeAreaView>
+  );
+};
+
+const styles = StyleSheet.create({});
+
+export default App;
